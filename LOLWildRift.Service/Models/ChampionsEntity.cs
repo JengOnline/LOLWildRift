f6bf9196c555f66b6134b24fec3ff6e4f7de363d
sophon jumpapang <sophonj@hatari.net>
2021-02-12 12:20:51 +0700

    Format the document

--- LOLWildRift.Service/Models/ChampionsEntity.cs
+++ LOLWildRift.Service/Models/ChampionsEntity.cs
@@ -1,66 +1,68 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
-namespace LOLWildRift.Service.Models
-{
-    public class ChampionsEntity
-    {
-        [JsonProperty("ID")]
+namespace LOLWildRift.Service.Models {
+    public class ChampionsEntity {
+        [JsonProperty ("ID")]
         public int ID { get; set; }
-        [JsonProperty("NAME")]
+
+        [JsonProperty ("NAME")]
         public string NAME { get; set; }
-        [JsonProperty("HISTORY")]
+
+        [JsonProperty ("HISTORY")]
         public string HISTORY { get; set; }
-        [JsonProperty("STATS_DAMAGE")]
+
+        [JsonProperty ("STATS_DAMAGE")]
         public int? STATS_DAMAGE { get; set; }
-        [JsonProperty("STATS_TOUGHNESS")]
+
+        [JsonProperty ("STATS_TOUGHNESS")]
         public int? STATS_TOUGHNESS { get; set; }
-        [JsonProperty("STATS_UTILITY")]
+
+        [JsonProperty ("STATS_UTILITY")]
         public int? STATS_UTILITY { get; set; }
-        [JsonProperty("STATS_DIFFICULITY")]
+
+        [JsonProperty ("STATS_DIFFICULITY")]
         public int? STATS_DIFFICULITY { get; set; }
-        [JsonProperty("ROLE")]
+
+        [JsonProperty ("ROLE")]
         public string ROLE { get; set; }
-        [JsonProperty("LANE")]
+
+        [JsonProperty ("LANE")]
         public string LANE { get; set; }
-        [JsonProperty("IMAGE_PATH")]
+
+        [JsonProperty ("IMAGE_PATH")]
         public string IMAGE_PATH { get; set; }
     }
 
-    public class ChampionsList
-    {
+    public class ChampionsList {
         private List<ChampionsEntity> _champions;
-        public ChampionsList()
-        {
-            _champions = new List<ChampionsEntity>();
+        public ChampionsList () {
+            _champions = new List<ChampionsEntity> ();
         }
 
-        public List<ChampionsEntity> Champions
-        {
+        public List<ChampionsEntity> Champions {
             get { return _champions; }
             set { value = _champions; }
         }
     }
 
-    public class ResultEntity
-    {
+    public class ResultEntity {
 
-        [JsonProperty("RESULT")]
+        [JsonProperty ("RESULT")]
         [Key]
         public bool RESULT { get; set; }
     }
 
-    public class ChampionAddEntity : ChampionsEntity
-    {
-        [JsonProperty("ROLE_ID")]
+    public class ChampionAddEntity : ChampionsEntity {
+        [JsonProperty ("ROLE_ID")]
         public int? ROLE_ID { get; set; }
 
-        [JsonProperty("RECOMMENDED_LANE_ID")]
+        [JsonProperty ("RECOMMENDED_LANE_ID")]
         public int? RECOMMENDED_LANE_ID { get; set; }
 
     }
--- LOLWildRift.Web/Models/ChampionsEntity.cs
+++ LOLWildRift.Web/Models/ChampionsEntity.cs
@@ -1,89 +1,79 @@
-﻿
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
-namespace LOLWildRift.Web.Models
-{
-    public class ChampionsEntity : ErrorEntity
-    {
-        [JsonProperty("ID")]
+namespace LOLWildRift.Web.Models {
+    public class ChampionsEntity : ErrorEntity {
+        [JsonProperty ("ID")]
         public int ID { get; set; }
 
-        [JsonProperty("NAME")]
+        [JsonProperty ("NAME")]
         public string NAME { get; set; }
 
-        [JsonProperty("HISTORY")]
+        [JsonProperty ("HISTORY")]
         public string HISTORY { get; set; }
 
-        [JsonProperty("STATS_DAMAGE")]
-        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [JsonProperty ("STATS_DAMAGE")]
+        [Range (1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
 
         public int? STATS_DAMAGE { get; set; }
 
-        [JsonProperty("STATS_TOUGHNESS")]
-        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [JsonProperty ("STATS_TOUGHNESS")]
+        [Range (1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int? STATS_TOUGHNESS { get; set; }
 
-        [JsonProperty("STATS_UTILITY")]
-        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [JsonProperty ("STATS_UTILITY")]
+        [Range (1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int? STATS_UTILITY { get; set; }
 
-        [JsonProperty("STATS_DIFFICULITY")]
-        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [JsonProperty ("STATS_DIFFICULITY")]
+        [Range (1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int? STATS_DIFFICULITY { get; set; }
 
-        [JsonProperty("ROLE")]
+        [JsonProperty ("ROLE")]
         public string ROLE { get; set; }
-        [JsonProperty("LANE")]
+
+        [JsonProperty ("LANE")]
         public string LANE { get; set; }
-        [JsonProperty("IMAGE_PATH")]
+
+        [JsonProperty ("IMAGE_PATH")]
         public string IMAGE_PATH { get; set; }
     }
 
-    public class ResultEntity 
-    {
+    public class ChampionsList : ErrorEntity {
+        private List<ChampionsEntity> _champions;
+        public ChampionsList () {
+            _champions = new List<ChampionsEntity> ();
+        }
 
-        [JsonProperty("RESULT")]
+        public List<ChampionsEntity> Champions {
+            get { return _champions; }
+            set { value = _champions; }
+        }
+    }
+
+    public class ResultEntity {
+
+        [JsonProperty ("RESULT")]
         [Key]
         public bool RESULT { get; set; }
     }
 
-    public class ChampionsList : ErrorEntity
-    {
-        private List<ChampionsEntity> _champions;
-        public ChampionsList()
-        {
-            _champions = new List<ChampionsEntity>();
-        }
-
-        public List<ChampionsEntity> Champions
-        {
-            get { return _champions; }
-            set { value = _champions; }
-        }
-
-
-    }
-
-    public class ChampionAddEntity : ChampionsEntity
-    {
-        [JsonProperty("ROLE_ID")]
+    public class ChampionAddEntity : ChampionsEntity {
+        [JsonProperty ("ROLE_ID")]
         public int? ROLE_ID { get; set; }
 
-        [JsonProperty("RECOMMENDED_LANE_ID")]
+        [JsonProperty ("RECOMMENDED_LANE_ID")]
         public int? RECOMMENDED_LANE_ID { get; set; }
 
-        [JsonProperty("IMAGE_FILE")]
+        [JsonProperty ("IMAGE_FILE")]
         public IFormFile IMAGE_FILE { get; set; }
     }
 
-    public class ErrorEntity
-    {
+    public class ErrorEntity {
         public bool Error { get; set; }
 
     }
-
 
 }
